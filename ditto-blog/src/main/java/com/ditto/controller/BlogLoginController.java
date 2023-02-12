cf7c51f40f57aeee1a50419709cde3ceb60f2702
zenas1023 <hanqi1023@gmail.com>
2023-02-12 18:44:18 +0800

    feat(Unified exception handling)

--- ditto-blog/src/main/java/com/ditto/controller/BlogLoginController.java
+++ ditto-blog/src/main/java/com/ditto/controller/BlogLoginController.java
@@ -2,7 +2,10 @@ package com.ditto.controller;
 
 import com.ditto.domain.ResponseResult;
 import com.ditto.domain.entity.User;
+import com.ditto.enums.AppHttpCodeEnum;
+import com.ditto.exception.SystemException;
 import com.ditto.service.BlogLoginService;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +18,10 @@ public class BlogLoginController {
     private BlogLoginService blogLoginService;
     @PostMapping("/login")
     public ResponseResult<?> login(@RequestBody User user) {
+        if (!StringUtils.hasText(user.getUserName())){
+            //提示 必须要传用户名
+            throw new SystemException(AppHttpCodeEnum.REQUIRE_USERNAME);
+        }
         return blogLoginService.login(user);
     }
 }
---
+++ ditto-framework/src/main/java/com/ditto/handler/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.ditto.handler.exception;
+
+import com.ditto.domain.ResponseResult;
+import com.ditto.enums.AppHttpCodeEnum;
+import com.ditto.exception.SystemException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(SystemException.class)
+    public ResponseResult<?> systemExceptionHandler(SystemException e){
+        //打印异常信息
+        log.error("出现了异常！ {}",e);
+        //从异常对象中获取提示信息封装返回
+        return ResponseResult.errorResult(e.getCode(),e.getMsg());
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseResult exceptionHandler(Exception e){
+        //打印异常信息
+        log.error("出现了异常！ {}",e);
+        //从异常对象中获取提示信息封装返回
+        return ResponseResult.errorResult(AppHttpCodeEnum.SYSTEM_ERROR.getCode(),e.getMessage());
+    }
+}
