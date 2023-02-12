14a4434d00fea654bfac08f4d3fa3e85298d3540
zenas1023 <hanqi1023@gmail.com>
2023-02-12 18:23:42 +0800

    feat(Authentication authorization exception handling)

--- ditto-blog/src/main/java/com/ditto/config/SecurityConfig.java
+++ ditto-blog/src/main/java/com/ditto/config/SecurityConfig.java
@@ -1,25 +1,41 @@
 package com.ditto.config;
 
+import com.ditto.filter.JwtAuthenticationTokenFilter;
+import com.ditto.handler.security.AuthenticationEntryPointImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.annotation.Resource;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Resource
+    private JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter;
+    @Resource
+    private AuthenticationEntryPoint authenticationEntryPoint;
+    @Resource
+    private AccessDeniedHandler accessDeniedHandler;
     @Override
     @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -31,11 +47,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 // 对于登录接口 允许匿名访问
                 .antMatchers("/login").anonymous()
+                .antMatchers("/link/getAllLink").authenticated()
                 // 除上面外的所有请求全部不需要认证即可访问
                 .anyRequest().permitAll();
 
+        //配置异常处理器
+        http.exceptionHandling()
+                        .authenticationEntryPoint(authenticationEntryPoint)
+                                .accessDeniedHandler(accessDeniedHandler);
 
         http.logout().disable();
+
+        http.addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
         //允许跨域
         http.cors();
     }
--- ditto-blog/src/main/java/com/ditto/controller/BlogLoginController.java
+++ ditto-blog/src/main/java/com/ditto/controller/BlogLoginController.java
@@ -16,6 +16,5 @@ public class BlogLoginController {
     @PostMapping("/login")
     public ResponseResult<?> login(@RequestBody User user) {
         return blogLoginService.login(user);
-    //
     }
 }
