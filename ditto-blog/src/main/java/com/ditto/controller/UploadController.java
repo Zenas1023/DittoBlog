a02e37642c4431c76532bb5e9221dbdc5f45955f
zenas1023 <hanqi1023@gmail.com>
2023-03-10 15:38:14 +0800

    feat(basic upload avatar)2

--- ditto-blog/src/main/java/com/ditto/controller/UploadController.java
+++ ditto-blog/src/main/java/com/ditto/controller/UploadController.java
@@ -1,6 +1,8 @@
 package com.ditto.controller;
 
 import com.ditto.domain.ResponseResult;
+import com.ditto.enums.AppHttpCodeEnum;
+import com.ditto.exception.SystemException;
 import com.ditto.service.UploadService;
 import com.ditto.vo.ResultVo;
 import org.springframework.http.*;
@@ -11,6 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.Base64;
 import java.util.Collections;
+import java.util.Objects;
 
 @RestController
 public class UploadController {
@@ -20,19 +23,11 @@ public class UploadController {
 
     @PostMapping("/upload")
     public void uploadFileToGitHub(MultipartFile img) throws IOException {
-        String url = String.format("https://api.github.com/repos/%s/%s/contents/%s", "Zenas1023", "imgBase", img.getOriginalFilename());
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-        headers.set("Authorization", "token " + "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");
-        String encodedContent = Base64.getEncoder().encodeToString(img.getBytes());
-        String body = String.format("{\"message\": \"Adding %s\", \"content\": \"%s\"}", img.getOriginalFilename(), encodedContent);
-        HttpEntity<String> entity = new HttpEntity<>(body, headers);
-        ResponseEntity<String> response = new RestTemplate().exchange(url, HttpMethod.PUT, entity, String.class);
-        if (response.getStatusCode() == HttpStatus.CREATED) {
-            System.out.println("File uploaded successfully!");
-        } else {
-            System.out.println("File upload failed with status code: " + response.getStatusCodeValue());
+        if (!Objects.requireNonNull(img.getOriginalFilename()).endsWith(".png")){
+            throw new SystemException(AppHttpCodeEnum.FILE_TYPE_ERROR);
+        }else {
+
         }
+        uploadService.upload(img);
     }
 }
--- ditto-framework/src/main/java/com/ditto/service/UploadService.java
+++ ditto-framework/src/main/java/com/ditto/service/UploadService.java
@@ -4,6 +4,9 @@ package com.ditto.service;
 import com.ditto.domain.ResponseResult;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 public interface UploadService {
 
+    void upload(MultipartFile img) throws IOException;
 }
