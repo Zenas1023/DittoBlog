7192a8842ea4850a6217cbb7a0904b1965e5e7ef
zenas1023 <hanqi1023@gmail.com>
2023-03-28 11:58:29 +0800

    feat

--- ditto-framework/src/main/java/com/ditto/service/impl/UserServiceImpl.java
+++ ditto-framework/src/main/java/com/ditto/service/impl/UserServiceImpl.java
@@ -1,15 +1,21 @@
 package com.ditto.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.ditto.domain.ResponseResult;
 import com.ditto.domain.entity.User;
 import com.ditto.domain.vo.UserInfoVo;
+import com.ditto.enums.AppHttpCodeEnum;
+import com.ditto.exception.SystemException;
 import com.ditto.mapper.UserMapper;
 import com.ditto.service.UserService;
 import com.ditto.utils.BeanCopyUtils;
 import com.ditto.utils.SecurityUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.util.Collection;
 import java.util.Map;
@@ -23,7 +29,8 @@ import java.util.function.Function;
  */
 @Service("userService")
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
-
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     @Override
     public ResponseResult<?> userInfo() {
         //获取当前用户id
@@ -40,5 +47,47 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
         updateById(user);
         return ResponseResult.okResult();
     }
-}
 
+    @Override
+    public ResponseResult<?> register(User user) {
+        //对数据进行非空判断
+        if(!StringUtils.hasText(user.getUsername())){
+            throw new SystemException(AppHttpCodeEnum.USERNAME_NOT_NULL);
+        }
+        if(!StringUtils.hasText(user.getPassword())){
+            throw new SystemException(AppHttpCodeEnum.PASSWORD_NOT_NULL);
+        }
+        if(!StringUtils.hasText(user.getEmail())){
+            throw new SystemException(AppHttpCodeEnum.EMAIL_NOT_NULL);
+        }
+        if(!StringUtils.hasText(user.getNickName())){
+            throw new SystemException(AppHttpCodeEnum.NICKNAME_NOT_NULL);
+        }
+        //对数据进行是否存在的判断
+        if(userNameExist(user.getUsername())){
+            throw new SystemException(AppHttpCodeEnum.USERNAME_EXIST);
+        }
+        if(nickNameExist(user.getNickName())){
+            throw new SystemException(AppHttpCodeEnum.NICKNAME_EXIST);
+        }
+        //...
+        //对密码进行加密
+        String encodePassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodePassword);
+        //存入数据库
+        save(user);
+        return ResponseResult.okResult();
+    }
+
+    private boolean nickNameExist(String nickName) {
+        LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(User::getNickName,nickName);
+        return count(queryWrapper) > 0;
+    }
+
+    private boolean userNameExist(String username) {
+        LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(User::getUsername,username);
+        return count(queryWrapper) > 0;
+    }
+}
